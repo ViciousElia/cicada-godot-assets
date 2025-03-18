7b404d5a30fa6a47967871e912311d746a64bdbd
Terra Hyde <terra@fruitfolio.com>
2025-03-18 17:55:27 -0600

    some more work on units

--- control/scripts/units_control.gd
+++ control/scripts/units_control.gd
@@ -1,11 +1,49 @@
+class_name UnitsControl
 extends VBoxContainer
 
+var disableAll : bool = false
+var textOnly : bool = false
+var fixedControl : bool = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+signal value_changed()
+signal settings_changed()
+
+func _ready(): pass
+func _process(_delta: float): pass
+func initialise(value : Variant, list : Variant = null, textUnit : bool = false, fixedSet : bool = false):
+	if fixedSet :
+		$TypeGroup/UnitsText.editable = false
+		$PickerGroup/EditButton.visible = false
+	textOnly = textUnit
+	fixedControl = fixedSet
+	if textUnit :
+		$PickerGroup.visible = false
+		$TypeGroup/ConfirmButton.visible = false
+		$TypeGroup.visible = true
+		$TypeGroup/UnitsText.text = value
+		return
+	$PickerGroup/UnitsOption.clear()
+	var newList = list
+	if list is String:
+		newList = _parse_as_units(list)
+	if newList is Array :
+		if newList.size() == 0:
+			$PickerGroup/UnitsOption.add_item("Units")
+			$PickerGroup/UnitsOption.select(0)
+			return
+		for text in newList:
+			$PickerGroup/UnitsOption.add_item(text)
+		$PickerGroup/UnitsOption.select(value)
+		return
+	$PickerGroup/UnitsOption.add_item("Units")
+	$PickerGroup/UnitsOption.select(0)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+
+
+
+func _parse_as_units(list : String) -> Array[String]:
+	var retVal : Array[String] = []
+	
+	return retVal
